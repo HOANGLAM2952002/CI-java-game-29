b7d3ecfdac01792a3fe92680d61dca464c04f883
HOANGLAM2952002 <tommy2952002@gmail.com>
2019-09-13 19:41:12 +0700

    .

---
+++ src/EnemyBlue.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.util.Random;
+
+public class EnemyBlue {
+    int move[] = {-5,5};
+    Image image;
+//    private Random rand;
+    Random rand = new Random();
+    int x = rand.nextInt(200);
+    int y = rand.nextInt(200);
+
+    public EnemyBlue(){
+        this.image = SpriteUtils.loadImage("assets/images/enemies/level0/blue/0.png");
+        this.x = rand.nextInt(200);
+        this.y = rand.nextInt(200);
+    }
+
+    public void render(Graphics g){
+        g.drawImage(this.image,x, y,null);
+    }
+
+    public void run(){
+        if (x == 0){
+            x += move[1];
+        }
+        if (x == 384){
+            x += move[0];
+        }
+        if (y == 0){
+            y += move[1];
+        }
+        if (y == 400){
+            y += move[0];
+        }
+        x += move[rand.nextInt(2)];
+        y += move[rand.nextInt(2)];
+    }
+}
